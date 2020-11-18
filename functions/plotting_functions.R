7b8bcff3720fd4cddf28409fb82735a6acaf66cd
clolesen <stoflo@gmail.com>
2020-11-18 12:30:47 +0100

    Timestep plot

--- functions/plotting_functions.R
+++ functions/plotting_functions.R
@@ -1,6 +1,22 @@
-colors = scale_color_manual(values=c("black", "#3498DB", "#E53935", "#27AE60", "#f0e442", "#490092"))
-fills = scale_fill_manual(values=c("black", "#3498DB", "#E53935", "#27AE60", "#f0e442", "#490092"))
+library(ggplot2)
+library(data.table)
+library(ggpubr)
+library(tidyverse)
 
+
+# colors used in plots
+# 1: Black
+# 2: Blue
+# 3: Red
+# 4: Green
+# 5: Yellow
+# 6: Purple
+color_palette = c("black", "#3498DB", "#E53935", "#27AE60", "#f0e442", "#490092")
+colors = scale_color_manual(values=palette)
+fills = scale_fill_manual(values=palette)
+
+
+#### AVERAGE PLOTS ####
 make_average_plot_data = function(data, variable){
   
   y = data[,..variable][[1]]
@@ -64,7 +80,7 @@ make_surprisal_matrix_plot = function(data, cond){
   return(plot)
 }
 
-plot_all_LOD_phi = function(data, task){
+make_all_LOD_phi_plot = function(data, task){
   
   plot_data = subset(data, task == task) 
   
@@ -78,3 +94,113 @@ plot_all_LOD_phi = function(data, task){
   
   return(plot)
 }
+
+
+#### TIMESTEP PLOTS ####
+
+make_timestep_plot_data = function(data, variables, runs, agents, trials){
+  
+  sub_data = subset(data, run %in% runs & agent %in% agents & trial %in% trials)
+  
+  columns = c("run", "agent", "trial", "timestep", variables)
+  
+  plot_data = sub_data[,..columns]
+  
+  return(plot_data)
+}
+
+normalize_surprisal = function(plot_data){
+  
+  n_columns = ncol(plot_data)
+  
+  # check for surprisal columns and divide them by 10
+  for (column in 5:n_columns){
+    name = names(plot_data[,..column]) 
+    split_name = strsplit(name, "_")[[1]]
+    if (split_name[1] == "surprisal") plot_data[,column] = plot_data[,..column]/5
+  }
+  
+  return(plot_data)
+}
+
+timestep_line_plot = function(plot_data, facet, variables) {
+  
+  n_columns = ncol(plot_data)
+  
+  plot = ggplot(plot_data, aes(x = timestep)) +
+    geom_segment(y =-.1, yend =-.1, x = 0, xend = 34, color = "black", size = 1) +
+    facet_wrap(as.formula(paste("~", facet)), scales = "free") +
+    theme_void() +
+    theme(
+      panel.grid.minor.x = element_line(colour = "lightgray", linetype = "dotted"),
+      strip.background = element_rect(colour = "white", fill = "#eeeeee"),
+      strip.text.x = element_text(margin = margin(3,0,3,0)),
+      panel.spacing = unit(1, "lines"),
+      plot.margin=unit(c(.5,.5,.5,.5), "cm")) +
+    scale_x_continuous(breaks = seq(1,33,1))
+
+  i = 2
+  for (var in variables){
+    plot = plot + geom_line(aes_string(y = var), color = color_palette[i], size=1)
+    i = i + 1
+  }
+  
+  return(plot)
+}
+
+convert_to_tile_plot_data = function(plot_data, variables){
+  
+  # Covert data to long format
+  tile_plot_data = gather(plot_data, y, value, variables)
+  
+  # convert to y axis values
+  scale_step = .2 # Size of vertical space for each block 
+  y_val = -.1 
+  for (var in variables){
+    y_val = y_val - scale_step
+    tile_plot_data[tile_plot_data[,"y"]==var,"y"] = y_val
+  }
+  tile_plot_data$y = as.numeric(tile_plot_data$y)
+  
+  return(tile_plot_data)
+}
+
+add_tile_to_timestep_plot = function(plot, plot_data, variables){
+  
+  tile_plot_data = convert_to_tile_plot_data(plot_data, variables)
+  
+  y_values = unique(tile_plot_data$y)
+  
+  for (value in y_values){
+    plot = plot + geom_segment(y = value, yend = value, x = -1.5, xend = 34, colour = "gray")
+  }
+  
+  plot = plot + 
+    geom_vline(xintercept=c(0,34), linetype="solid", color = "black") +
+    theme(legend.position = "none", axis.text.x = element_text(size = 6), axis.text.y = element_text(size = 8)) +
+    scale_alpha_continuous(range = c(0,1)) +
+    scale_y_continuous(breaks = y_values, labels = variables, limits = c(min(y_values), 3)) +
+    geom_tile(data = tile_plot_data,
+              aes(y = y, alpha = value),
+              height = .1)
+                     
+  return(plot)           
+}
+
+make_timestep_plot = function(data, line_variables, tile_variables, facet, runs = 0:49, agents = 0:120, trials = 0:127){
+  
+  #LINE PLOT
+  line_plot_data = make_timestep_plot_data(data, line_variables, runs, agents, trials)
+  
+  line_plot_data = normalize_surprisal(line_plot_data)
+  
+  line_plot = timestep_line_plot(line_plot_data, facet, line_variables)
+  
+  # TILE PLOT
+  tile_plot_data = make_timestep_plot_data(data, tile_variables, runs, agents, trials)
+  
+  full_plot = add_tile_to_timestep_plot(line_plot, tile_plot_data, tile_variables)
+  
+  return(full_plot)
+}
+
