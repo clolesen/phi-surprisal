6b214949461adf001de4a4007e09d85d51f92f42
PTWaade <peter@waade.net>
2021-04-23 20:52:52 +0200

    made surprisal calculator

--- python_utils/get_goal_prior_distribution.py
+++ python_utils/get_goal_prior_distribution.py
@@ -1,5 +1,6 @@
 #import libraries
 import pandas as pd
+import numpy as np
 import pickle
 
 #Only for writing code
@@ -20,21 +21,26 @@ def get_goal_prior_distribution(task=4):
     #Read data
     timestep_data = pd.read_csv('processed_data/timestep_data_task{}_info.csv'.format(task))
 
-    #Subset data for only perfect agents on the last generation
-    timestep_data = timestep_data.loc[(timestep_data['run'].isin(perfect_runs)) & (timestep_data['agent'] == 49)]
-
     #Create sensory state and context state
     timestep_data['sensory_state'] = timestep_data['S1'].astype('str') + timestep_data['S2'].astype('str')
-    timestep_data['context'] = timestep_data['timestep'].astype('str') + timestep_data['task_type'] + timestep_data['block_movement'].astype('str')
 
-    #Count how many times each state appears in each context
-    counts = timestep_data.groupby('context')['sensory_state'].value_counts() + 1
-
-    #And make it into a dicitonary
-    distribution_dict = counts.to_dict()
-
-    #Save the goal prior distribution to a file
-    with open('processed_data/goal_prior_distribution.pkl', 'wb') as handle:
-        pickle.dump(distribution_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+    #Subset data for only perfect agents on the last generation
+    timestep_data_perfect = timestep_data.loc[(timestep_data['run'].isin(perfect_runs)) & (timestep_data['agent'] == 49)]
     
+    #Count how many times each state appeared in each context
+    counts = timestep_data_perfect.groupby(['timestep', 'task_type', 'block_movement', 'sensory_state'])['sensory_state'].agg(surprisal='count')
+
+    #Add 1 to all counts to include unobserved states
+    counts = counts.unstack(fill_value=0).stack() + 1
+
+    #Transform counts into probabilities
+    goal_prior_surprisal = counts.groupby(['timestep', 'task_type', 'block_movement']).apply(lambda x: - np.log(x / float(x.sum())) ).reset_index()
+
+    #Save the distribution for later plotting 
+    goal_prior_surprisal.to_pickle('goal_prior_distribution.pkl')
+
+    #Merge into the original dataset
+    timestep_data = timestep_data.merge(goal_prior_surprisal, how = "left")
+
+    #Save data to csv
+    timestep_data.to_csv('processed_data/timestep_data_task{}_surprise.csv'.format(task), index=False)
