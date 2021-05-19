43303570cda56fd3d13ff0930fdf8028e8278401
PTWaade <peter@waade.net>
2021-05-19 17:37:40 +0200

    preprocessing scripts finished

--- prepare_data_master.py
+++ prepare_data_master.py
@@ -31,28 +31,3 @@ calculate_surprisal(task=task)
 
 # Caluclate Phi
 calculate_phi(task=task)
-
-
-
-
-
-import pickle        
-import base64
-import csv
-
-your_pickle_obj = pickle.loads(open('processed_data/goal_prior_distribution_task1.pkl', 'rb').read())
-with open('processed_data/goal_prior_distribution_task1.csv', 'a', encoding='utf8') as csv_file:
-    wr = csv.writer(csv_file, delimiter='|')
-    pickle_bytes = pickle.dumps(your_pickle_obj)            # unsafe to write
-    b64_bytes = base64.b64encode(pickle_bytes)  # safe to write but still bytes
-    b64_str = b64_bytes.decode('utf8')          # safe and in utf8
-    wr.writerow(['col1', 'col2', b64_str])
-
-
-import pickle as pkl
-import pandas as pd
-with open("processed_data/goal_prior_distribution_task1.pkl", "rb") as f:
-    object = pkl.load(f)
-    
-df = pd.DataFrame(object)
-df.to_csv(r'processed_data/goal_prior_distribution_task1.csv')
